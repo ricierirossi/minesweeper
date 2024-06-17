a956de67d8d221dd4f325c9bee38ab7ddeaa3fde
Matheus Ricieri <matheus.ricieri@hotmail.com>
2024-06-17 20:29:08 -0300

    Create BoardComponent.vue

---
+++ src/components/BoardComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Minesweeper</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+div {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+  width: 100%;
+
+  background-color: #303030;
+  color: whitesmoke;
+}
+</style>
