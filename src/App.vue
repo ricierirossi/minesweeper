e458d29eeba56dcc243cbf7a474773fafa6ff50c
Matheus Ricieri <matheus.ricieri@hotmail.com>
2024-06-19 18:49:56 -0300

    Add stopwatch function
    Add stopwatch function and passed it as an event to ScoreBoard.vue

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ const bombs = ref(10);
 const cells = ref([]);
 const numberOfCells = rows.value * column.value;
 const actionsCounter = ref(0);
+const time = ref({ hours: 0, minutes: 0, seconds: 0, maxValueOfTime: 60 });
 
 function initializeCells() {
   for (let cellIndex = 0; cellIndex++; cellIndex <= numberOfCells) {
@@ -28,16 +29,33 @@ function initializeCells() {
 function countActions() {
   actionsCounter.value += 1;
 }
+
+function stopwatch() {
+  setInterval(() => {
+    time.value.seconds++;
+
+    if (time.value.seconds === time.value.maxValueOfTime) {
+      time.value.seconds = 0;
+      time.value.minutes++;
+    }
+
+    if (time.value.minutes === time.value.maxValueOfTime) {
+      time.value.minutes = 0;
+      time.value.hours++;
+    }
+  }, 1000);
+}
 </script>
 
 <template>
   <body>
     <div class="game">
       <BoardComponent />
-      <ScoredBoard :actions-counter="actionsCounter" />
+      <ScoredBoard :actions-counter="actionsCounter" :time="time" />
       <CellsComponent
         :number-of-cells="numberOfCells"
         @count-actions-event="countActions"
+        @click.once="stopwatch"
       />
     </div>
   </body>
