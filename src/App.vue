69f31810bcd66826e45e2f8d960c8eb47b154f87
Matheus Ricieri <matheus.ricieri@hotmail.com>
2024-06-20 19:39:26 -0300

    Add funcionalities
    Refactor stopwatch event receiving
    Add lifecycle hook to initialize cells data
    Add functions to place bombs on cells pseudo-randomly
    Pass cells data to CellsComponent

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
 import "./assets/App.css";
 
-import BoardComponent from "./components/BoardComponent.vue";
 import CellsComponent from "./components/CellsComponent.vue";
 import ScoredBoard from "./components/ScoreBoard.vue";
 
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 
 const rows = ref(10);
 const column = ref(10);
@@ -16,13 +15,26 @@ const actionsCounter = ref(0);
 const time = ref({ hours: 0, minutes: 0, seconds: 0, maxValueOfTime: 60 });
 
 function initializeCells() {
-  for (let cellIndex = 0; cellIndex++; cellIndex <= numberOfCells) {
-    cells.push({
+  for (let cellIndex = 0; cellIndex < numberOfCells; cellIndex++) {
+    cells.value.push({
       revealed: false,
       flagged: false,
       bomb: false,
       adjacentBombs: 0,
+      cellID: cellIndex,
     });
+  }
+  placeBombs();
+}
+
+function placeBombs() {
+  let placedBombs = 0;
+  while (placedBombs < bombs.value) {
+    let chosenCellID = Math.floor(Math.random() * numberOfCells);
+    if (!cells.value[chosenCellID].bomb) {
+      cells.value[chosenCellID].bomb = true;
+      placedBombs++;
+    }
   }
 }
 
@@ -45,17 +57,21 @@ function stopwatch() {
     }
   }, 1000);
 }
+
+onMounted(() => {
+  initializeCells();
+});
 </script>
 
 <template>
   <body>
     <div class="game">
-      <BoardComponent />
       <ScoredBoard :actions-counter="actionsCounter" :time="time" />
       <CellsComponent
         :number-of-cells="numberOfCells"
+        :cells="cells"
         @count-actions-event="countActions"
-        @click.once="stopwatch"
+        @start-stopwatch-event.once="stopwatch"
       />
     </div>
   </body>
